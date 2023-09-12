583121e4137c3b07396002717f6beea9883de32e
Wen Zhen <wlzeign@hotmail.com>
2023-09-12 15:19:53 +0800

    reformat

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from '@/store/counterSlice'
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "@/store/counterSlice";
+import selectionReducer from "@/store/selectionSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer
+    counter: counterReducer,
+    selection: selectionReducer,
   },
-})
+});
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
