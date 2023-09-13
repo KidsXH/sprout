e4d45ddc48bfe2d53826abc1dafc768c4c975db7
Wen Zhen <wlzeign@hotmail.com>
2023-09-13 20:38:24 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,16 +8,17 @@ import Core from "../components/core";
 
 export default function Home() {
   return (
-    <main className="flex h-screen w-full flex-col items-center justify-center mx-auto px-4">
-      <Core />
-      <div className="flex flex-col w-[96rem] h-[60rem] overflow-clip px-4">
-        <div className="flex flex-row w-full h-[32rem]">
-          {/* <ModelViewer /> */}
+    <main className="mx-auto flex h-screen w-full flex-col overflow-auto p-4 2xl:items-center">
+      <div className="flex w-[96rem] items-center justify-end 2xl:mt-16">
+        <Core />
+      </div>
+      <div className="flex h-[60rem] w-[96rem] flex-col px-4">
+        <div className="flex h-[32rem] w-full flex-row">
           <CodeView />
           <ChainVis />
           <TextView />
         </div>
-        <div className="flex flex-row w-full">
+        <div className="flex w-full flex-row">
           <VisView />
         </div>
       </div>
