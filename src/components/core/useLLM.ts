4b54deb47b14abe5a0398562f91f188a8e067e86
Wen Zhen <wlzeign@hotmail.com>
2023-09-15 15:39:41 +0800

    Update useLLM.ts

--- src/components/core/useLLM.ts
+++ src/components/core/useLLM.ts
@@ -5,13 +5,15 @@ import { Planner } from "@/models/agents/planner";
 
 export const useLLM = () => {
   const apiKey = useAppSelector((state) => state.model.apiKey);
+  const modelName = useAppSelector((state) => state.model.modelName);
   const [model, setModel] = useState<BaseModel | null>(null);
   const [planner, setPlanner] = useState<Planner | null>(null);
 
   useEffect(() => {
     const newModel = new BaseModel(apiKey);
+    newModel.model = modelName;
     setModel(newModel);
-  }, [apiKey]);
+  }, [apiKey, modelName]);
 
   useEffect(() => {
     if (model) {
