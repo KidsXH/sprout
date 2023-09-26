7f4a01a7dfc6c5a9c62fde4ba5978578a21828b2
Yihan Liu <lyh1024@zju.edu.cn>
2023-09-26 18:00:22 +0800

    add: config panel style

--- src/components/SpaceView/configPanel.tsx
+++ src/components/SpaceView/configPanel.tsx
@@ -1,11 +1,55 @@
 "use client";
 import React from "react";
 import * as d3 from "d3";
+import Slider from "@mui/material/Slider";
+import { styled } from "@mui/material/styles";
 import { useEffect, useState } from "react";
+const styles = ["Academic", "Humorous", "Objective", "Other"];
+
+const PrettoSlider = styled(Slider)({
+  color: "#52af77",
+  height: 6,
+  "& .MuiSlider-track": {
+    border: "none",
+  },
+  "& .MuiSlider-thumb": {
+    height: 12,
+    width: 12,
+    backgroundColor: "#fff",
+    border: "2px solid currentColor",
+    "&:focus, &:hover, &.Mui-active, &.Mui-focusVisible": {
+      boxShadow: "inherit",
+    },
+    "&:before": {
+      display: "none",
+    },
+  },
+  "& .MuiSlider-valueLabel": {
+    lineHeight: 1.2,
+    fontSize: 12,
+    background: "unset",
+    padding: 0,
+    width: 32,
+    height: 32,
+    borderRadius: "50% 50% 50% 0",
+    backgroundColor: "#52af77",
+    transformOrigin: "bottom left",
+    transform: "translate(50%, -100%) rotate(-45deg) scale(0)",
+    "&:before": { display: "none" },
+    "&.MuiSlider-valueLabelOpen": {
+      transform: "translate(50%, -100%) rotate(-45deg) scale(1)",
+    },
+    "& > *": {
+      transform: "rotate(45deg)",
+    },
+  },
+});
 
 export const ConfigPanel = () => {
-  const svg = d3.select("#node-space");
-  svg.selectAll("*").remove();
+  const [sentenceValue, setSentenceValue] = useState(4);
+  const [style, setStyle] = useState("Academic");
+
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
   const bigRectWidth = 150;
   const bigRectHeight = 72;
@@ -17,60 +61,74 @@ export const ConfigPanel = () => {
     range: [2, 4],
     text: "node",
   };
-  svg
+  const handleClick = () => {};
 
-    .append("rect")
-    .attr("class", "branch-node-shadow")
-    .attr("x", (d) => 0)
-    .attr("y", (d, i) => 0)
-    .attr("width", (d) => bigRectWidth)
-    .attr("height", (d) => bigRectHeight)
-    .attr("fill", (d) => nodeRectData.color)
-    .attr("rx", 16)
-    .attr("ry", 16);
+  useEffect(() => {
+    const svg = d3.select("#node-space");
+    svg.selectAll("*").remove();
+    svg
 
-  svg
+      .append("rect")
+      .attr("class", "branch-node-shadow")
+      .attr("x", (d) => 0)
+      .attr("y", (d, i) => 0)
+      .attr("width", (d) => bigRectWidth)
+      .attr("height", (d) => bigRectHeight)
+      .attr("fill", (d) => nodeRectData.color)
+      .attr("rx", 16)
+      .attr("ry", 16);
 
-    .append("rect")
-    .attr("class", "branch-node")
-    .attr("x", (d) => 0)
-    .attr("y", (d) => 0)
-    .attr("width", (d) => bigRectWidth)
-    .attr("height", (d) => bigRectHeight - 5)
-    .attr("fill", "#f5f5f5")
-    .attr("rx", 14)
-    .attr("ry", 14);
+    svg
 
-  //render branch node text
-  svg
+      .append("rect")
+      .attr("class", "branch-node")
+      .attr("x", (d) => 0)
+      .attr("y", (d) => 0)
+      .attr("width", (d) => bigRectWidth)
+      .attr("height", (d) => bigRectHeight - 5)
+      .attr("fill", "#f5f5f5")
+      .attr("rx", 14)
+      .attr("ry", 14);
 
-    .append("rect")
-    .attr("class", "branch-node-text select-none")
-    .attr("x", (d) => 0 + 15)
-    .attr("y", (d, i) => 0 + textOffsetY)
-    .attr("text-anchor", "start")
-    .attr("fill", "#000")
-    .attr("font-size", "14px")
-    .text((d) => nodeRectData.text);
+    //render branch node text
+    svg
 
-  //render code range
-  svg
+      .append("rect")
+      .attr("class", "branch-node-text select-none")
+      .attr("x", (d) => 0 + 15)
+      .attr("y", (d, i) => 0 + textOffsetY)
+      .attr("text-anchor", "start")
+      .attr("fill", "#000")
+      .attr("font-size", "14px")
+      .text((d) => nodeRectData.text);
 
-    .append("text")
-    .attr("class", "code-range-text select-none")
-    .attr("x", (d) => 0 + codeRangeOffsetX)
-    .attr("y", (d, i) => 0 + codeRangeOffsetY)
-    .attr("fill", "#000")
-    .attr("font-size", "14px")
-    .attr("text-anchor", "start")
-    .text(
-      (d) =>
-        nodeRectData.range[0] +
-        (nodeRectData.range[0] === nodeRectData.range[1]
-          ? ""
-          : "-" + nodeRectData.range[1]),
-    );
+    //render code range
+    svg
 
+      .append("text")
+      .attr("class", "code-range-text select-none")
+      .attr("x", (d) => 0 + codeRangeOffsetX)
+      .attr("y", (d, i) => 0 + codeRangeOffsetY)
+      .attr("fill", "#000")
+      .attr("font-size", "14px")
+      .attr("text-anchor", "start")
+      .text(
+        (d) =>
+          nodeRectData.range[0] +
+          (nodeRectData.range[0] === nodeRectData.range[1]
+            ? ""
+            : "-" + nodeRectData.range[1]),
+      );
+  }, []);
+
+  useEffect(() => {
+    const buttons = document.getElementsByClassName("style-button");
+    buttons[selectedIndex].classList.add("selectedStyleButtonStyle");
+
+    return () => {
+      buttons[selectedIndex].classList.remove("selectedStyleButtonStyle");
+    };
+  }, [selectedIndex]);
   return (
     <div className="m-3 mt-0 flex flex-col">
       <div className="flex flex-row">
@@ -83,36 +141,71 @@ export const ConfigPanel = () => {
         ></svg>
         <div className="flex w-20 flex-col">
           <div className="m-1 text-center">Type</div>
-          <div className="flex h-9 w-20 items-center justify-center rounded-md border-2 border-solid border-black bg-green-100">
-            Content
+          <div className="flex h-8 w-20 items-center justify-center  border-2 border-solid border-black bg-green-100 text-xs">
+            Explanation
           </div>
         </div>
       </div>
       <div className="h-full w-full rounded-md border-2 border-solid border-gray-100">
         <div className="polish m-2">
           <div className="p-1 text-base">Polishing</div>
-          <div className="p-1 text-sm text-gray-400"> Make it more</div>
+          <div className="p-1 text-sm text-gray-600"> Make it more</div>
           <div className="grid grid-flow-col grid-rows-2 gap-3">
-            <div className="flex h-5  w-full  items-center justify-center text-xs underline decoration-green-100 decoration-solid decoration-4 underline-offset-1">
-              Confirm
-            </div>
-            <div className="flex  h-5  w-full items-center justify-center text-xs">
-              Confirm
-            </div>
-            <div className="flex  h-5  w-full items-center justify-center text-xs">
-              Confirm
-            </div>
-            <div className="flex  h-5  w-full items-center justify-center text-xs">
-              Confirm
-            </div>
+            {styles.map((v, i) => (
+              <div
+                className="style-button flex h-5  w-full  items-center justify-center text-xs text-slate-500 underline decoration-gray-200 decoration-solid decoration-4 underline-offset-0"
+                onClick={() => {
+                  setSelectedIndex(i);
+                  setStyle(v);
+                }}
+              >
+                {v}
+              </div>
+            ))}
+            {/* <div className="flex h-5  w-full items-center justify-center ">
+              <input
+                className=" h-5  w-10 text-xs text-slate-500 underline decoration-gray-200 decoration-solid decoration-4 underline-offset-0"
+                type="text"
+                defaultValue={"?"}
+              />
+            </div> */}
+
+            {/* <div
+              className="flex h-5  w-full  items-center justify-center text-xs text-slate-500 underline decoration-gray-200 decoration-solid decoration-4 underline-offset-0"
+              onClick={() => {
+                // setStyle(v);
+              }}
+            >
+              {"  ?  "}
+            </div> */}
           </div>
-          <div className="p-1 text-base">Length</div>
-          <input className="w-10 border-2 border-gray-200" type="text" />
-          <span className="p-1 text-sm text-gray-400">sentences</span>
+          <div className="mt-1 p-1 text-base">Length</div>
+          <div className="flex flex-row">
+            {" "}
+            <PrettoSlider
+              className="pl-2"
+              defaultValue={4}
+              marks={true}
+              min={1}
+              max={11}
+              step={2}
+              onChange={(e, v) => setSentenceValue(v as number)}
+            />
+            {/* <input className="w-10 border-2 border-gray-200 p-1" type="text" /> */}
+            <span className="flex items-center justify-center pl-2 text-sm">
+              {sentenceValue}
+            </span>
+            <span className="flex items-center justify-center pl-2 text-sm text-gray-400">
+              sentences
+            </span>
+          </div>
         </div>
-        <div className="lengthm-2"></div>
+
         <div className="confirmButton m-2 flex items-center justify-center">
-          <div className="flex  h-7  w-16 items-center justify-center rounded-md border-2 border-gray-200 bg-gray-100 text-xs">
+          <div
+            className="flex  h-7  w-16 items-center justify-center rounded-md border-2 border-gray-200 bg-gray-100 text-xs"
+            onClick={handleClick}
+          >
             Confirm
           </div>
         </div>
