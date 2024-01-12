60991f29d9f368ae6748a13936dda60c704e796b
ollierwoodman <ollierwoodman@gmail.com>
2024-01-13 01:53:27 +0800

    fix map call

--- src/models/embeddings/index.ts
+++ src/models/embeddings/index.ts
@@ -1,54 +1,54 @@
-// import { CreateEmbeddingResponse } from "openai";
-// import { EmbeddingModel } from "@/models/api";
-// import { UMAP } from "umap-js";
+import { CreateEmbeddingResponse } from "openai";
+import { EmbeddingModel } from "@/models/api";
+import { UMAP } from "umap-js";
 
-// export async function getCoordinates(
-//   inputs: string[],
-//   apiKey: string,
-// ): Promise<number[][] | undefined> {
-//   if (inputs.length < 1) {
-//     return new Promise(() => {
-//       return [];
-//     });
-//   }
+export async function getCoordinates(
+  inputs: string[],
+  apiKey: string,
+): Promise<number[][] | undefined> {
+  if (inputs.length < 1) {
+    return new Promise(() => {
+      return [];
+    });
+  }
 
-//   if (inputs.includes("")) {
-//     return new Promise(() => {
-//       return [];
-//     });
-//   }
+  if (inputs.includes("")) {
+    return new Promise(() => {
+      return [];
+    });
+  }
 
-//   const embedModel = new EmbeddingModel(apiKey, "text-embedding-ada-002");
-//   const embeddingsResponse = embedModel
-//     .getEmbeddings(inputs)
-//     .catch((err) => {
-//       console.error(
-//         "Error while retrieving node embeddings from OpenAI: " + err,
-//       );
-//     })
-//     .then((res: CreateEmbeddingResponse | void) => {
-//       if (!res) {
-//         return;
-//       }
+  const embedModel = new EmbeddingModel(apiKey, "text-embedding-ada-002");
+  const embeddingsResponse = embedModel
+    .getEmbeddings(inputs)
+    .catch((err) => {
+      console.error(
+        "Error while retrieving node embeddings from OpenAI: " + err,
+      );
+    })
+    .then((res: CreateEmbeddingResponse | void) => {
+      if (!res) {
+        return;
+      }
 
-//       const embeddings = res.data.map((value: any, index: any) => {
-//         console.assert(index == value.index);
-//         return value.embedding;
-//       });
+      const embeddings = res.map((value: any, index: any) => {
+        console.assert(index == value.index);
+        return value.embedding;
+      });
 
-//       // console.log("embeddings: ", embeddings);
+      // console.log("embeddings: ", embeddings);
 
-//       const umap = new UMAP({
-//         nComponents: 2,
-//         nNeighbors: 1,
-//       });
-//       if (embeddings.length <= 1) {
-//         const umapResult = umap.fit(embeddings.concat(embeddings));
-//         return umapResult;
-//       }
-//       const umapResult = umap.fit(embeddings);
-//       return umapResult;
-//     });
+      const umap = new UMAP({
+        nComponents: 2,
+        nNeighbors: 1,
+      });
+      if (embeddings.length <= 1) {
+        const umapResult = umap.fit(embeddings.concat(embeddings));
+        return umapResult;
+      }
+      const umapResult = umap.fit(embeddings);
+      return umapResult;
+    });
 
-//   return embeddingsResponse;
-// }
+  return embeddingsResponse;
+}
